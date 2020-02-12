3b913f42ac5b825f7df09a5f226070e4c9bd1c28
Madiyar <nu_ofsn11@mail.ru>
2020-02-12 10:52:32 +0600

    Simple primer

---
+++ primeri/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace primeri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userAnswer;
+            string[] exercises = { "1 + 1","1 - 1","1 * 1","1 / 1"};
+            string[] rightAnswers = { "2", "0", "1", "1" };
+            int[] rightAnswersInt = { 2,0,1,1 };
+            int count = 0;
+            Dictionary<string, int> exercises2 = new Dictionary<string, int>();
+            
+            for(int i = 0; i < exercises.Length; i++)
+            { 
+                
+                    Console.WriteLine($"{i + 1}) " + exercises[i] + " = ");
+                    userAnswer = Console.ReadLine();
+                    if (userAnswer == rightAnswers[i]) { count++; }
+                
+                
+                
+            }
+
+            Console.WriteLine("Right answers: " + count);
+            Console.WriteLine("Wrong answers: " + (exercises.Length-count));
+
+
+            Console.ReadKey();
+
+        }
+        static void ShowCol(string[] array)
+        {
+
+        }
+    }
+}
