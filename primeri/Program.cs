97df3a06f824a9fc15be32ca30546ec4b938f458
Madiyar <nu_ofsn11@mail.ru>
2020-02-13 11:48:45 +0600

    Primeri

--- primeri/Program.cs
+++ primeri/Program.cs
@@ -12,17 +12,34 @@ namespace primeri
             string[] rightAnswers = { "2", "0", "1", "1" };
             int[] rightAnswersInt = { 2,0,1,1 };
             int count = 0;
+            int handledAnswer = 0;
             Dictionary<string, int> exercises2 = new Dictionary<string, int>();
             
             for(int i = 0; i < exercises.Length; i++)
             { 
-                
+                while (true)
+                {
                     Console.WriteLine($"{i + 1}) " + exercises[i] + " = ");
                     userAnswer = Console.ReadLine();
-                    if (userAnswer == rightAnswers[i]) { count++; }
-                
-                
-                
+                    try {
+                        handledAnswer = Convert.ToInt32(userAnswer);
+                        
+                        if (handledAnswer == rightAnswersInt[i]) { count++; }
+                        break;
+                    }
+                    catch (FormatException e)
+                    {
+                        if (userAnswer == "") Console.WriteLine("You entered nothing");
+                        else Console.WriteLine("Asnwer should be number");
+                    }
+                    
+                    
+                    
+
+                }
+
+
+
             }
 
             Console.WriteLine("Right answers: " + count);
