71d05dd094568a5f398dd063c278176b09f1252e
Madiyar <nu_ofsn11@mail.ru>
2020-02-17 10:54:33 +0600

    some changes

--- primeri/Program.cs
+++ primeri/Program.cs
@@ -14,61 +14,85 @@ namespace primeri
             string[] isRight = new string[exercises.Length];
             int count = 0;
             int handledAnswer = 0;
-            Dictionary<string, int> exercises2 = new Dictionary<string, int>();
-            
+            bool enabled = true;
+            Dictionary<string, int> quan = new Dictionary<string, int>();
 
-            for (int i = 0; i < exercises.Length; i++)
+            for(int i = 0; i < exercises.Length; i++)
             {
+                quan.Add(exercises[i], rightAnswersInt[i]);  
+            }
+            Array.Sort(exercises);
+            ShowCol(quan);
+            
+            while (enabled)
+            {
+                for (int i = 0; i < exercises.Length; i++)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine($"{i + 1}) " + exercises[i] + " = ");
+                        userAnswer = Console.ReadLine();
+                        try
+                        {
+                            handledAnswer = Convert.ToInt32(userAnswer);
+                            userAnswers[i] = userAnswer;
+                            // correctness of user answer
+                            if (handledAnswer == rightAnswersInt[i])
+                            {
+                                isRight[i] = "Correct";
+                                count++;
+                            }
+                            else isRight[i] = "Incorrect";
+
+                            break;
+                        }
+                        catch (FormatException)
+                        {
+                            if (userAnswer == "") Console.WriteLine("You entered nothing");
+                            else Console.WriteLine("Asnwer should be number");
+                        }
+                    }
+                }
+                Console.WriteLine("Right answers: " + count);
+                Console.WriteLine("Wrong answers: " + (exercises.Length - count));
+
+                string showWork;
+                string someResult = "";
+
                 while (true)
                 {
-                    Console.WriteLine($"{i + 1}) " + exercises[i] + " = ");
-                    userAnswer = Console.ReadLine();
-                    try
+                    Console.WriteLine("Do you want to see your work?");
+                    showWork = Console.ReadLine();
+                    if (showWork == "y")
                     {
-                        handledAnswer = Convert.ToInt32(userAnswer);
-                        userAnswers[i] = userAnswer;
-                        if (handledAnswer == rightAnswersInt[i])
+                        for (int i = 0; i < exercises.Length; i++)
                         {
-                            isRight[i] = "Correct";
-                            count++;
+                            if (isRight[i] == "Incorrect") someResult = $"Right answer: {rightAnswersInt[i]}";
+                            else someResult = "";
+                            Console.WriteLine($"{i + 1}) {exercises[i]} = {userAnswers[i]} {isRight[i]}. {someResult}");
                         }
-                        else isRight[i] = "Incorrect";
-                        
                         break;
                     }
-                    catch (FormatException)
-                    {
-                        if (userAnswer == "") Console.WriteLine("You entered nothing");
-                        else Console.WriteLine("Asnwer should be number");
-                    }
+                    else if (showWork == "n") break;
                 }
-            }
-            Console.WriteLine("Right answers: " + count);
-            Console.WriteLine("Wrong answers: " + (exercises.Length - count));
 
-            string showWork;
-            string someResult = "";
-            while (true)
-            {
-                Console.WriteLine("Do you want to see your work?");
-                showWork = Console.ReadLine();
-                if (showWork == "y")
+                while (true)
                 {
-                    for (int i = 0; i < exercises.Length; i++)
-                    {
-                        if (isRight[i] == "Incorrect") someResult = $"Right answer: {rightAnswersInt[i]}";
-                        else someResult = "";
-                        Console.WriteLine($"{i + 1}) {exercises[i]} = {userAnswers[i]} {isRight[i]}. {someResult}");
-
-
-                    }
-                    break;
+                    Console.WriteLine("Again?");
+                    string again = Console.ReadLine();
+                    if (again == "y") break;
+                    else if (again == "n") 
+                        {
+                        enabled = false;
+                        Console.WriteLine("Buy!");
+                        break;
+                        }
                 }
-                else if (showWork == "n") break;
+
+
             }
 
             Console.ReadKey();
-
 
         }
         static void ShowCol(string[] array)
@@ -76,6 +100,13 @@ namespace primeri
             foreach (string el in array)
             {
                 Console.WriteLine(el);
+            }
+        }
+        static void ShowCol(Dictionary<string,int> array)
+        {
+            foreach (KeyValuePair<string,int> el in array)
+            {
+                Console.WriteLine(el.Key + " " + el.Value);
             }
         }
     }
